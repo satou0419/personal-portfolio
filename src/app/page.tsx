1b71bea40c868910792150d5cc74611ec53ffa4d
Rey Dante Garcia <116933977+satou0419@users.noreply.github.com>
2025-09-24 10:34:26 +0800

    PPW-2-[Component][Navigation][Banner] (#1)
    * PPW-2-[Component][Navigation]
    
    -Added navigation bar component [Testing]
    
    * PPW-3-[Component][Banner]
    
    -Implemented banner component
    -Implemented light & darkmode
    
    * -Added additional changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,61 @@
+import './globals.css'
+import './homepage.scss'
+import './banner.scss'
 import Image from "next/image";
-import styles from "./page.module.css";
+import { FaLinkedinIn, FaSquareFacebook, FaSquareGithub } from 'react-icons/fa6'
 
-export default function Home() {
+export default function Homepage() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="homepage">
+      {/* Navigation */}
+      <nav className="navbar">
+       <a href="/" className="navbar__logo">
+  <img src="./assets/images/logo-light.svg" alt="ReyDev Logo" className="logo-light" />
+  <img src="./assets/images/logo-dark.svg" alt="ReyDev Logo" className="logo-dark" />
+</a>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+
+        <ul className="navbar__menu">
+          <li className="navbar__item"><a href="#home" className="navbar__link">Home</a></li>
+          <li className="navbar__item"><a href="#projects" className="navbar__link">Projects</a></li>
+          <li className="navbar__item"><a href="#contact" className="navbar__link">Contact</a></li>
+        </ul>
+      </nav>
+
+      {/* Banner */}
+      <section className="banner">
+        <div className="banner__info">
+          <div className="banner__details">
+            <p className="banner__intro">Hi I&apos;m</p>
+            <h1 className="banner__title">Rey Dante G. Garcia</h1>
+            <p className="banner__desc">
+              An IT enthusiast that aspires to immerse in the ins and outs of front-end development.
+              Creating compelling websites using creative visuals and handy tools that surpass expectations.
+            </p>
+          </div>
+
+          <div className="banner__socials">
+            <a><FaSquareFacebook/></a>
+            <a><FaSquareGithub/></a>
+            <a><FaLinkedinIn/></a>
+          </div>
+          
+          <div className="banner__buttons">
+            <button className="btn btn--primary">View Project</button>
+            <button className="btn btn--secondary">Message Me</button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+        <div className="banner__image">
+            <Image
+            src="/assets/images/profile.jpg"
+            alt="Profile of Rey Dante G. Garcia"
+            width={280}
+            height={280}
+            className="profile-image"
+            priority/>
+        </div>
+      </section>
+    </main>
+  )
 }
